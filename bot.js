70ef40a5408b769aeebd4d6d73be35e1807f070e
Seventty <elmastigeron2015@gmail.com>
2020-12-03 14:07:30 -0400

    Weather command (/clima) added

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ Welcome to the 7th.
 // Import zone //
 import TelegramBot from 'node-telegram-bot-api';
 import dotenv from 'dotenv';
-import weather from 'weather-js';
+import axios from 'axios';
 
 // Bot engine / instance //
 dotenv.config();
@@ -18,22 +18,18 @@ bot.on('polling_error', error=>{
 });
 
 // -- First-order Commands -- // 
-
-bot.onText(/^\/heya/, msg => {
-	bot.sendMessage(msg.chat.id, "Pendiente a todas las ordenes & lista para recibir un comando");
-})
-
-bot.onText(/^\/info/, msg => {
-	let title = process.title,
-		version = process.versions.node,
-		modules = process.versions.modules,
-		openssl = process.versions.openssl,
-		platform = process.platform,
-		pid = process.pid,
-		ppid = process.ppid;
-	bot.sendMessage(msg.chat.id, `🔰System info🔰\n\nRunning in: ${title} 🍃\nVersion: ${version}\nModules: ${modules}\nOpenSSL ${openssl}\nLiving in: ${platform}\nPID: ${pid} READY to kill\nPPID: ${ppid}\nBot version: 0.0.1 s.e.e.d 🌱`);
+// Greetings
+bot.onText(/^\/heya/, message => {
+	bot.sendMessage(message.chat.id, "Pendiente a todas las ordenes & lista para recibir un comando");
 });
 
+// Repeat everything you type
+bot.onText(/\/rp (.+)/, (message, value) => {
+  bot.sendMessage(message.chat.id, `${value[1]}`);
+});
+
+
+// Dice game
 bot.onText(/^\/dado (.+)/, (message, value) => {
 	if(!isNaN(value[1]) && value[1] < 7){	
 		bot.sendMessage(message.chat.id, `Elegiste: *${value[1]}*`,{parse_mode : "Markdown"});
@@ -46,20 +42,45 @@ bot.onText(/^\/dado (.+)/, (message, value) => {
 	}else{bot.sendMessage(message.chat.id, "Introduce un numero del 1-6")}
 });
 
-//Still testing, not working.
-bot.onText(/^\!clima (.+)/, (message, value) => {
-	let config_payload = {
-		search : value[1], // City to search
-		degreeType : 'C', // Temperature format, it can be F too (Celcius or Fahrenheit)
-		lang : 'es-ES' // Language code es = spanish
-	}
-	 weather.find(config_payload, (err, res) => {
-		if(err){
-			bot.sendMessage(message.chat.id, `Intenta escribir la ciudad de manera correcta`);
-		}else{
-			bot.sendMessage(message.chat.id, `He encontrado algo.`);
-		}
-	 });
+// Weather command
+bot.onText(/\/clima (.+)/, (message, value) => {
+	const payload = {
+		token : "appid=4bd7d205123e24502a47a7812095629d",
+		unit : "units=metric",
+		lang : "lang=es"
+	};
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${value[1]}&${payload.unit}&${payload.token}&${payload.lang}`;
+	const getWeather = async url => {
+		try{
+				const res = await axios.get(url.replace(/ /g, "%20"));
+				bot.sendMessage(message.chat.id,
+						` ${res.data.name}, ${res.data.sys.country}\n
+*Temperatura:* ${res.data.main.temp}
+*Temperatura a sentir:* ${res.data.main.feels_like}
+*Temperatura maxima:* ${res.data.main.temp_max}
+*Temperatura minima:* ${res.data.main.temp_min}
+*Humedad: * ${res.data.main.humidity}%
+*Descripcion* ${res.data.weather[0].description}
+					`,
+					{parse_mode : "Markdown"}
+				);
+			}catch(err){
+				bot.sendMessage(message.chat.id, `Introduce una ciudad correcta, eje: Buenos Aires`);
+			}
+	};
+	getWeather(url);
+});
+
+// Info about the bot
+bot.onText(/^\/info/, message  => {
+	let title = process.title,
+		version = process.versions.node,
+		modules = process.versions.modules,
+		openssl = process.versions.openssl,
+		platform = process.platform,
+		pid = process.pid,
+		ppid = process.ppid;
+	bot.sendMessage(message.chat.id, `🔰System info🔰\n\nRunning in: ${title} 🍃\nVersion: ${version}\nModules: ${modules}\nOpenSSL ${openssl}\nLiving in: ${platform}\nPID: ${pid} READY to kill\nPPID: ${ppid}\nBot version: 0.0.1 s.e.e.d 🌱`);
 });
 
 // -- Second-order Commands -- Events//
