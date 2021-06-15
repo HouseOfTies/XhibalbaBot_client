ab18f105b9708ce525c5dd010e7ff5709f0348da
seventty <elmastigeron2015@gmail.com>
2021-06-15 01:37:40 -0400

    changes
    info command created
    
    first query launched to the database

--- app/model/bot.js
+++ app/model/bot.js
@@ -4,6 +4,7 @@ Welcome to the 7th.
 import TelegramBot from 'node-telegram-bot-api';
 import dotenv from 'dotenv';
 import fs from 'fs';
+import express from 'express';
 
 // --- Key Chain --- //
 dotenv.config({ path: '../../.env'});
@@ -11,6 +12,8 @@ const TelegramBotToken = process.env.remote_bot_TOKEN || process.env.local_bot_T
 
 // --  Bot engine / instance -- //
 const bot = new TelegramBot(TelegramBotToken, {polling: true}); //It will take heroku TOKEN or localToken
+const PORT = 8080;
+const HOST = '0.0.0.0';
 
 // - Errors detector - //
 bot.on('polling_error', error => {
@@ -19,6 +22,43 @@ bot.on('polling_error', error => {
 	console.log(error);
 });
 
-console.log("\nWelcome to the 7th, bot running.\n");
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Welcome to 7th inside system, this message is to show the bot is running already.');
+});
+
+app.listen(PORT, HOST);
+console.log(`\nBot running on http://${HOST}:${PORT}`, '\nWelcome to the 7th, bot running.\n');
 
 
+bot.onText(/\?info/, (message) => {
+	const { chat: { id }, from } = message;
+
+	const userTemplate = {
+		id: "",
+		username: "",
+		first_name: "",
+		last_name: "",
+		language_code: "",
+		is_bot: "",
+	};
+	
+	const user = {};
+
+	for (let i in userTemplate) {
+		from[i] === undefined ? user[i] = "Nothing" : user[i] = from[i];
+	}
+
+	const { username, first_name, last_name, language_code, is_bot } = user;
+  
+	if(!is_bot) bot.sendMessage(id, 
+			`
+			User info:
+			|*ID*: ${user.id}
+			|*Username*: ${username}
+			|*First Name*: ${first_name}
+			|*Last Name*: ${last_name}
+			|*Language*: ${language_code}
+			`, {reply_to_message_id: id, parse_mode: "Markdown"}
+		);
+});
