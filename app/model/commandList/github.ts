6008567c34bade77c8a5d69cbeb15399b830a167
Seventty <elmastigeron2015@gmail.com>
2021-08-13 22:47:00 -0400

    update

--- app/model/commandList/github.ts
+++ app/model/commandList/github.ts
@@ -1,3 +1,9 @@
-export default function github(){
-
+export default async function github(user: string, repository: string){
+    let url: string = "";
+    if(!repository){
+        url = `https://api.github.com/users/${user}`;
+    }else{
+        url = `https://api.github.com/repos/${user}/${repository}`;
+    }
+    const res = await fetch(url);
 }
