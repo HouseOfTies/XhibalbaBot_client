7adb1bc173f4d3b36ea77efbaf4ae90bd87f33f1
seventty <elmastigeron2015@gmail.com>
2021-09-24 20:57:33 -0400

    update

--- src/bot.ts
+++ src/bot.ts
@@ -1,18 +1,18 @@
 import TelegramBot from 'node-telegram-bot-api';
 import config from './config';
 
-console.log(config);
+const bot: TelegramBot = new TelegramBot(config.bot, {polling: true});
 
-
-//const bot: TelegramBot = new TelegramBot(token, {polling: true});
-
-/* bot.on('polling_error', error => {
-	console.log(error);
+bot.on('polling_error', error => {
+  console.log(error);
 });
 
 console.log("\nConnected with the darkness of 7th\n");
 
+bot.onText(/^\/heya/, function(message){
+  bot.sendMessage(message.chat.id, `Greetings 👋🏻 ${message.from.username}`);
+});
 
 bot.on('message', function (message) {
     console.log(message);
-  }); */
+});
