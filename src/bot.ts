31b40c66696bbabeb74f0d03ae983579e199dfeb
seventty <elmastigeron2015@gmail.com>
2021-09-25 08:29:07 -0400

    update

--- src/bot.ts
+++ src/bot.ts
@@ -1,18 +1,39 @@
-import TelegramBot from 'node-telegram-bot-api';
+import 'reflect-metadata'; // We need this in order to use @Decorators
+
 import config from './config';
 
-const bot: TelegramBot = new TelegramBot(config.bot, {polling: true});
+import express from 'express';
 
-bot.on('polling_error', error => {
+import TelegramBot from 'node-telegram-bot-api';
+
+async function startBot(){
+  const bot: TelegramBot = new TelegramBot(config.bot, {polling: true});
+  const app = express();
+
+  bot.on('polling_error', error => {
   console.log(error);
-});
+  });
+
+  bot.on('message', message => {
+    console.log(message);
+  });
+
+  console.log(`
+      ###############################################
+          🔰  Xhiba listening on port: ${config.port} 🔰
+      ###############################################
+  `);
+}
+
+startBot();
+/* 
 
 console.log("\nConnected with the darkness of 7th\n");
 
 bot.onText(/^\/heya/, function(message){
-  bot.sendMessage(message.chat.id, `Greetings 👋🏻 ${message.from.username}`);
+  bot.sendMessage(message.chat.id, `Greetings 👋🏻`);
 });
 
 bot.on('message', function (message) {
     console.log(message);
-});
+}); */
