7f5be0c2bf9f647112caea4b5ba4ac1189e4e8e3
seventty <elmastigeron2015@gmail.com>
2021-09-29 19:08:05 -0400

    docker compose updated, config updated too

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-if(process.env.NODE_ENV == "development"){
+if(process.env.NODE_ENV !== "production"){
   const envFound = dotenv.config();
   if (envFound.error) {
     throw new Error("⚠️  Couldn't find .env file  ⚠️");
