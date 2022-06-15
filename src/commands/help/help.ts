50cc7e3f18538fc5e7b8620d6dfa253fd2e80545
seventty <elmastigeron2015@gmail.com>
2022-06-15 00:08:22 -0400

    new commands, service and job

--- src/commands/help/help.ts
+++ src/commands/help/help.ts
@@ -1,83 +1,114 @@
 import { v4 as uuidv4 } from "uuid";
 const helpBook = require("./helpbook.json");
 
-let defaultIndex: number = 1;
+/* let defaultIndex: number = 1;
+let templateStringResponse: string = `${helpBook.name} ${helpBook.icon}\n`;
+const buttonId: string = uuidv4();
+
+const buttons: any = {
+  reply_markup: {
+    inline_keyboard: [
+      [
+        {
+          text: "⬅️ Atrás",
+          callback_data: "back " + buttonId,
+        },
+        {
+          text: "Siguiente ➡️",
+          callback_data: "next " + buttonId,
+        },
+      ],
+      [
+        {
+          text: "Cerrar libro ❌",
+          callback_data: "close " + buttonId,
+        },
+      ],
+    ],
+  },
+  parse_mode: "Markdown",
+};
 
 export default function help(bot: any, message: any) {
   const chatId = message.chat.id,
     messageId = message.message_id;
   const userId: number = message.from.id;
-  const buttonId: string = uuidv4();
-  let templateString: string = `Libro de ayuda ${helpBook.icon}\n`;
 
-  const buttons: any = {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: "⬅️ Atrás",
-            callback_data: "back " + buttonId,
-          },
-          {
-            text: "Siguiente ➡️",
-            callback_data: "next " + buttonId,
-          },
-        ],
-        [
-          {
-            text: "Cerrar libro ❌",
-            callback_data: "close " + buttonId,
-          },
-        ],
-      ],
-    },
-  };
+  bot.sendMessage(
+    chatId,
+    templateStringResponse +
+      `*Indice: ${defaultIndex}*\n--------\n` +
+      commandList(defaultIndex),
+    buttons
+  );
 
-  bot.sendMessage(chatId, templateString, buttons, {
-    reply_to_message_id: messageId,
-  });
+  bot.on(
+    "callback_query",
+    function onCallbackQuery(actionButton) {
+      const data: any = actionButton.data.split(" ")[0];
+      const buttonIdToTrigger: string = actionButton.data.split(" ")[1];
+      const actionTriggeredByUserId: number = actionButton.from.id;
+      const actionTriggeredInChatId: number = actionButton.message.chat.id;
 
-  bot.on("callback_query", function onCallbackQuery(actionButton) {
-    const data: any = actionButton.data.split(" ")[0];
-    const buttonIdToTrigger: string = actionButton.data.split(" ")[1];
-    const actionTriggeredByUserId: number = actionButton.from.id;
-    const actionTriggeredInChatId: number = actionButton.message.chat.id;
-
-    if (
-      actionTriggeredByUserId == userId &&
-      actionTriggeredInChatId == chatId &&
-      buttonIdToTrigger == buttonId
-    ) {
-      switch (data) {
-        case "back":
-          /*if (defaultIndex > 1) {
-              bot.editMessageText(`${templateString}Indice: ${actualIndex}\n--------\n`, {
-              chat_id: chatId,
-              message_id: messageId + 1,
-              reply_markup: buttons.reply_markup,
-            });
-          }
-             */
-          if(defaultIndex > 1){
-            defaultIndex--;
-            console.log("Indice restado");  
-          }else{
-            console.log("No puedo restar mas");
-          }
-          break;
-        case "next":
-          if(defaultIndex < helpBook.totalIndexes){
-            defaultIndex++;
-            console.log("Indice sumado");
-          }else{
-            console.log("No puedo sumar mas");
-          }
-          break;
-        case "close":
-          bot.deleteMessage(chatId, messageId + 1);
-          break;
+      if (
+        actionTriggeredByUserId == userId &&
+        actionTriggeredInChatId == chatId &&
+        buttonIdToTrigger == buttonId
+      ) {
+        switch (data) {
+          case "back":
+            if (defaultIndex > 1) {
+              defaultIndex--;
+              updateBotMessageState(bot, actionButton.message.message_id - 1, chatId);
+            }
+            break;
+          case "next":
+            if (defaultIndex < helpBook.totalIndexes) {
+              defaultIndex++;
+              updateBotMessageState(bot, actionButton.message.message_id - 1, chatId);
+            }
+            break;
+          case "close":
+            defaultIndex = 1;
+            bot.deleteMessage(chatId, messageId + 1);
+            break;
+        }
       }
-      console.log("Index actual: ", defaultIndex);
+    },
+    { cache_time: 1 }
+  );
+}
+
+const updateBotMessageState = (bot: any, messageId: number, chatId: number) => {
+  let newStringResponse: string = `*Indice: ${defaultIndex}*\n--------\n`;
+  console.log(defaultIndex);
+  console.log(messageId);
+  bot.editMessageText(
+    templateStringResponse + newStringResponse + commandList(defaultIndex),
+    {
+      chat_id: chatId,
+      message_id: messageId + 1,
+      reply_markup: buttons.reply_markup,
+      parse_mode: "Markdown",
     }
-  });
+  );
+};
+
+const commandList = (index) => {
+  const { icon, name, commands } = helpBook.indexes[index];
+  let returnString = `*${name}* ${icon}\n\n`;
+
+  for (var property of Object.keys(commands)) {
+    returnString += `⚜️ *${property}*: ${commands[property]}\n\n`;
+  }
+
+  return returnString;
+};
+ */
+
+export default function help(bot: any, message: any){
+  const chatId = message.chat.id,
+    messageId = message.message_id;
+  const userId: number = message.from.id;
+  bot.sendMessage(chatId, "Volvemos a lo basico");
 }
--- src/loaders/commands.ts
+++ src/loaders/commands.ts
@@ -1,29 +1,53 @@
 import Logger from './logger';
-import { heya, say, rsay, fsay, help, loader } from '@/commands';
+import { heya, say, rsay, fsay, help, loader, github, imageSearcher, snapshot, weather, wikipedia, youtube } from '@/commands';
 
 export default async ({ bot }) => {
     Logger.info("Commands loaded ✅");
     
     // Example command
-    bot.onText(/^\/heya/, async (message) => {
+    bot.onText(/^\/heya/, async message => {
     // Whole programming logic within this section
         heya(bot, message);
     });
 
-    bot.onText(/^\/say/, async (message) => {
+    bot.onText(/^\/say/, async message => {
         say(bot, message);
     });
 
-    bot.onText(/^\/rsay/, async (message) => {
+    bot.onText(/^\/rsay/, async message => {
         rsay(bot, message);
     });
 
-    bot.onText(/^\/fsay/, async (message) => {
+    bot.onText(/^\/fsay/, async message => {
         fsay(bot, message);
     });
 
     bot.onText(/^\/help/, async message => {
         help(bot, message);
+    });
+
+    bot.onText(/^\/github/, async message => {
+        github(bot, message);
+    });
+
+    bot.onText(/^\/img/, async message => {
+        imageSearcher(bot, message);
+    });
+
+    bot.onText(/^\/snap/, async message => {
+        snapshot(bot, message);
+    });
+
+    bot.onText(/(?:^\/weather|clima)/, async message => {
+        weather(bot, message);
+    });
+
+    bot.onText(/^\/wiki/, async message => {
+        wikipedia(bot, message);
+    });
+
+    bot.onText(/^\/yt/, async message => {
+        youtube(bot, message);
     });
 
     /* bot.onText(/^\/load/, async message => {
