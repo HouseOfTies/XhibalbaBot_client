ff7ca037e6da1a31ce3a7e193c21eae7c4a575fa
seventty <elmastigeron2015@gmail.com>
2022-08-13 15:23:37 -0400

    Main test

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import "reflect-metadata"; // We need this in order to use @Decorators
+/* import "reflect-metadata"; // We need this in order to use @Decorators
 import config from "./config";
 import express from "express";
 import TelegramBot from "node-telegram-bot-api";
@@ -47,3 +47,43 @@ async function startBot() {
 }
 
 startBot();
+ */
+
+const TOKEN = process.env.TELEGRAM_TOKEN || '998778278:AAEYTCar7G2hGUYAS04Viz402MbysR0Y-oc';
+const url = 'https://kerathroswebhook-production.up.railway.app';
+const port = 443;
+
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+
+// No need to pass any parameters as we will handle the updates with Express
+const bot = new TelegramBot(TOKEN);
+
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+const app = express();
+
+// parse the updates to JSON
+app.use(express.json());
+
+app.get(`/`, (req, res) => {
+  res.send("Hello world");
+});
+
+// We are receiving updates at the route below!
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+  console.log(req)
+});
+
+// Start Express Server
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
+
+// Just to ping!
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'I am alive!');
+});
