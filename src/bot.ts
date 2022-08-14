75a7973391c1420696d9947982667625989a6f39
seventty <elmastigeron2015@gmail.com>
2022-08-13 21:36:15 -0400

    testing main branch

--- src/bot.ts
+++ src/bot.ts
@@ -5,41 +5,50 @@ import TelegramBot from "node-telegram-bot-api";
 import Logger from "./loaders/logger";
 
 async function startBot() {
-  const bot: TelegramBot = new TelegramBot(config.bot, {
-    polling: process.env.NODE_ENV === "production" ? false : true,
-  });
-
-  bot.setWebHook(`${config.url}/${config.bot}`);
+  let bot: TelegramBot;
+  if (process.env.NODE_ENV === "production") {
+    bot = new TelegramBot(config.bot);
+    bot.setWebHook(`${config.url}/${config.bot}`);
+  } else {
+    bot = new TelegramBot(config.bot, { polling: true });
+  }
   const app = express();
   const motd = `-----------------------------------------------
                 🔰 Xhiba listening on port: ${config.port} 🔰
+                Running in ${
+                  process.env.NODE_ENV === "production"
+                    ? "Production"
+                    : "Development"
+                } environment
         -----------------------------------------------`;
-        bot.setWebHook(`${config.url}/${config.bot}`);
-        
-        // parse the updates to JSON
-        app.use(express.json());
-        
-        app.get(`/`, (req, res) => {
-          res.send("Powered by express");
-        });
-        
-        // We are receiving updates at the route below!
-        app.post(`/${config.bot}`, (req, res) => {
-          bot.processUpdate(req.body);
-          res.sendStatus(200);
-        });
-        
-        // Start Express Server
-        app.listen(config.port, () => {
-          console.log(`Express server is listening on ${config.port}`);
-        });
-        
-        // Just to ping!
-        bot.on('message', msg => {
-          console.log(msg);
-        });
+  bot.setWebHook(`${config.url}/${config.bot}`);
 
+  // parse the updates to JSON
+  app.use(express.json());
+
+  app.get(`/`, (req, res) => {
+    res.send("Powered by express");
+  });
+
+  // We are receiving updates at the route below!
+  app.post(`/${config.bot}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+
+  // Start Express Server
+  app.listen(config.port, async () => {
+    console.log(motd);
+    await require("./loaders/commands").default({
+      bot: bot,
+    });
+  });
+
+  bot.on("message", (msg) => {
+    console.log(msg);
+  });
 }
+
 /* const bot: TelegramBot = new TelegramBot(config.bot, {
     polling: process.env.NODE_ENV === "production" ? false : true,
   });
